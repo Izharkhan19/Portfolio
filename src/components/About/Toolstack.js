5569d404eafe76b2b795d59657f4354ebfa304d3
Izharkhan2022 <izhar.khan@softqubes.com>
2024-02-05 19:19:00 +0530

    Create new portfolio

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiVercel,
+  SiMacos,
+  SiSwagger,
+  SiNotepadplusplus,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiMacos />
+      </Col> */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNotepadplusplus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSwagger />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
